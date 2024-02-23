a624d54e133b993fed90cd0e5417d778a091438f
NGUYEN HOAI VY B2014811 <vyb2014811@student.ctu.edu.vn>
2024-02-23 16:21:00 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -11,5 +13,16 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book applicaton." });
 });
+
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 module.exports = app;
